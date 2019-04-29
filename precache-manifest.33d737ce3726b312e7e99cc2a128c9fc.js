f50b09fde25c8b135ea02dd11a1943615156e0ca
tungstensol <jobs@tungstensol.com>
2019-04-30 02:46:04 +0530

    v1

---
+++ precache-manifest.33d737ce3726b312e7e99cc2a128c9fc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5ef8717af56b881158cd468a1c27bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e409206b591ef3b41ce9",
+    "url": "/static/css/main.9a0c8ad7.chunk.css"
+  },
+  {
+    "revision": "eb209cd09d531ead25b7",
+    "url": "/static/js/2.4bc68df7.chunk.js"
+  },
+  {
+    "revision": "e409206b591ef3b41ce9",
+    "url": "/static/js/main.d29549db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bc135108618a034aa5c773d525f79e30",
+    "url": "/static/media/BlueCollar.bc135108.jpeg"
+  },
+  {
+    "revision": "0d14ddcee521ade2259fa2850df7b059",
+    "url": "/static/media/IT.0d14ddce.jpeg"
+  },
+  {
+    "revision": "53dcf6f56556158e1ea588f31f45b62e",
+    "url": "/static/media/TempStaffing.53dcf6f5.jpeg"
+  },
+  {
+    "revision": "97d1407fa83fad726e2467286a4adb86",
+    "url": "/static/media/about.97d1407f.jpeg"
+  },
+  {
+    "revision": "41a02c12a7cc989690ffcdc4af26957d",
+    "url": "/static/media/executive.41a02c12.jpeg"
+  },
+  {
+    "revision": "f77847c8f66db871986fe99b6fb5ad3e",
+    "url": "/static/media/logo.f77847c8.jpeg"
+  }
+]);
